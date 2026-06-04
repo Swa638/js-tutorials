a843da157740c2f5f2aaf18fa75c36416afbba1d
Swapnil Patil <swapnilpati312004@gmail.com>
2026-06-04 13:00:16 +0530

    First commit

---
+++ demo.js
@@ -0,0 +1,83 @@
+/* var x;
+x = 10;
+console.log(x, typeof x)
+var x;
+x = "swapnil";
+console.log(x)
+var x = "swapnil "
+console.log(x,typeof x) */
+/* let x;
+ x = 10;
+console.log(x);
+let x;
+x = "swapnil";
+console.log(x); */
+/* const x = 10;
+console.log(x); 
+x = 25;
+console.log(x); */
+
+// operators
+/* 1. arithmetic operators
+var x = 5;
+var y = 5;
+console.log(x + y);
+var x = 5;
+var y = 5;
+console.log(x - y);
+var x = 5;
+var y = 5;
+console.log(x * y);
+var x = 5;
+var y = 5;
+console.log(x ** y);
+var x = 5;
+var y = 2;
+console.log(x / y);
+var x = 5;
+var y = 5;
+console.log(x % y);
+var x = 5;
+x++;
+console.log(x)
+
+var x = 5;
+x--;
+console.log(x) */
+
+/* assignment operator 
+var x = 5;
+x = x + 2;
+console.log(x)
+var x = 5;
+x  = x - 3;
+console.log(x)
+var x = 5;
+x  = x * 2;
+console.log(x) */
+ 
+/* comparision operator 
+var x = 5;
+var y = 2;
+console.log(x > y)
+var x = 5;
+var y = 2;
+console.log(x < y)
+var x = 5;
+var y = 2;
+console.log(x >= y)
+var x = 5;
+var y = 2;
+console.log(x <= y)
+var x = 5;
+var y = 2;
+console.log(x == y)
+var x = 5;
+var y = 2;
+console.log(x === y)
+var x = 5;
+var y = 2;
+console.log(x != y) */
+// logical operators //
+
+
