60540e066ede81ed4c1ba7c65ac77034bf51fb1a
Swapnil Patil <swapnilpati312004@gmail.com>
2026-06-05 11:48:44 +0530

    loop.js

---
+++ loop.js
@@ -0,0 +1,33 @@
+/* console.log("hi");
+var x = 0;
+while(x<3)
+{
+    console.log("hello",x)
+    x++
+}
+console.log("bye");
+*/
+/* var x = 2;
+var i = 1;
+while(i<=10)
+{
+    console.log(x,"x",i,"=",x*i);
+    i++;
+}*/
+
+
+/* for loop
+var x = 3;
+for(var i = 1; i<=10; i++)
+{
+    console.log(x,"x",i,"=",x*i);
+} */
+
+    // do while //
+    var i = 0;
+    do{
+        console.log("hello",i);
+        i++;
+
+    }
+    while(i<3);
